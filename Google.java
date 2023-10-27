d428950487c426472260b25f39ceb06e1d7753c1
vanimadevxworkz <vanimadev@gmail.com>
2023-10-27 11:25:21 +0530

    bbb

---
+++ Google.java
@@ -0,0 +1,371 @@
+package com.xworkz.test.things;
+
+public class Google {
+private String userName;
+private String password;
+private String mailId;
+private long contactNumber;
+private int notification;
+private int numberOfMessages;
+private boolean profile;
+private String ceo;
+private String founded;
+private String headquarters;
+private String cmo;
+private String founders;
+private int numberOfFounders;
+private String subsidiaries;
+private String parentOrganizationName;
+private boolean isSafety;
+private boolean color;
+private int blockwebsite;
+private int allowwebsite;
+private int numberOfContacts;
+private boolean storage;
+private int numberOfGooglePhotos;
+private short numberOfGoogleAccounts;
+private short totalApps;
+private boolean isVersion;
+private String versionName;
+private int loginTime;
+private String userName1;
+private String password1;
+private String mailId1;
+private long contactNumber1;
+private int notification1;
+private int numberOfMessages1;
+private boolean profile1;
+private String ceo1;
+private String founded1;
+private String headquarters1;
+private String cmo1;
+private String founders1;
+private int numberOfFounders1;
+private String subsidiaries1;
+private String parentOrganizationName1;
+private boolean isSafety1;
+private boolean color1;
+private int blockwebsite1;
+private int allowwebsite1;
+private int numberOfContacts1;
+private boolean storage1;
+private int numberOfGooglePhotos1;
+private short numberOfGoogleAccounts1;
+private short totalApps1;
+private boolean isVersion1;
+
+
+public void setUserName(String name) {
+	this.userName=name;
+}
+public void setPassword(String password) {
+	this.password=password;
+}
+public void setMailId(String mailId) {
+	this.mailId=mailId;
+}
+public void setContactNumber(long conatactNumber) {
+	this.contactNumber=contactNumber;
+}
+public void setNotification(int notification) {
+	this.notification=notification;
+}
+public void setNmberOfMessages(int numberOfMessages) {
+	this.numberOfMessages=numberOfMessages;
+}
+public void setProfile(boolean profile) {
+	this.profile=profile;
+}
+public void setCeo(String ceo) {
+	this.ceo=ceo;
+}
+public void setFounded(String founded) {
+	this.founded=founded;
+}
+public void setHeadquartes(String headquarters) {
+	this.headquarters=headquarters;
+}
+public void setCmo(String cmo) {
+	this.cmo=cmo;
+}
+public void setfounders(String founders) {
+	this.founders=founders;
+}
+public void setNumberOfFounders(int numberOfFounders) {
+	this.founders=founders;
+}
+public void setSubsidiaries(String subsidiaries) {
+	this.subsidiaries=subsidiaries;
+}
+public void setParentOrganizationName(String parentOrganizationName) {
+	this.parentOrganizationName=parentOrganizationName;
+}
+public void setIsSafety(boolean isSafety) {
+	this.isSafety=isSafety;
+}
+public void setColor(boolean color) {
+	this.color=color;
+}
+public void setBlockwebsite(int blockwebsite) {
+	this.blockwebsite=blockwebsite;
+}
+public void setAllowwebsite(int allowwebsite) {
+	this.allowwebsite=allowwebsite;
+}
+public void setNumberOfContacts(int numberOfontacts) {
+	this.numberOfContacts=numberOfContacts;
+}
+public void setStorage(boolean storage) {
+	this.storage=storage;
+}
+public void setNumberOfGooglePhotos(int numberOfGooglePhotos) {
+	this.numberOfGooglePhotos=numberOfGooglePhotos;
+}
+public void setNumberOfGoogleAccounts(short numberOfGoogleAccounts) {
+	this. numberOfGoogleAccounts= numberOfGoogleAccounts;
+}
+public void setTotalApps(short totalApps) {
+	this.totalApps=totalApps;
+}
+public void setIsVersion(boolean isVersion) {
+	this.isVersion=isVersion;
+}
+public void setVersionName(String versionName) {
+	this.versionName=versionName;
+}
+public void setLoginTime(int loginTime) {
+	this.loginTime=loginTime;
+}
+public void setUserName1(String name1) {
+	this.userName1=name1;
+}
+public void setPassword1(String password1) {
+	this.password1=password1;
+}
+public void setMailId1(String mailId1) {
+	this.mailId1=mailId1;
+}
+public void setContactNumber1(long conatactNumber1) {
+	this.contactNumber1=contactNumber1;
+}
+public void setNotification1(int notification1) {
+	this.notification1=notification1;
+}
+public void setNmberOfMessages1(int numberOfMessages1) {
+	this.numberOfMessages1=numberOfMessages1;
+}
+public void setProfile1(boolean profile1) {
+	this.profile1=profile1;
+}
+public void setCeo1(String ceo1) {
+	this.ceo1=ceo1;
+}
+public void setFounded1(String founded1) {
+	this.founded1=founded1;
+}
+public void setHeadquartes1(String headquarters1) {
+	this.headquarters1=headquarters1;
+}
+public void setCmo1(String cmo1) {
+	this.cmo1=cmo1;
+}
+public void setfounders1(String founders1) {
+	this.founders1=founders1;
+}
+public void setNumberOfFounders1(int numberOfFounders1) {
+	this.founders1=founders1;
+}
+public void setSubsidiaries1(String subsidiaries1) {
+	this.subsidiaries1=subsidiaries1;
+}
+public void setParentOrganizationName1(String parentOrganizationName1) {
+	this.parentOrganizationName1=parentOrganizationName1;
+}
+public void setIsSafety1(boolean isSafety1) {
+	this.isSafety1=isSafety1;
+}
+public void setColor1(boolean color1) {
+	this.color1=color1;
+}
+public void setBlockwebsite1(int blockwebsite1) {
+	this.blockwebsite1=blockwebsite1;
+}
+public void setAllowwebsite1(int allowwebsite1) {
+	this.allowwebsite1=allowwebsite1;
+}
+public void setNumberOfContacts1(int numberOfontacts1) {
+	this.numberOfContacts1=numberOfContacts1;
+}
+public void setStorage1(boolean storage1) {
+	this.storage1=storage1;
+}
+public void setNumberOfGooglePhotos1(int numberOfGooglePhotos1) {
+	this.numberOfGooglePhotos1=numberOfGooglePhotos1;
+}
+public void setNumberOfGoogleAccounts1(short numberOfGoogleAccounts1) {
+	this. numberOfGoogleAccounts1= numberOfGoogleAccounts1;
+}
+public void setTotalApps1(short totalApps1) {
+	this.totalApps1=totalApps1;
+}
+public void setIsVersion1(boolean isVersion1) {
+	this.isVersion1=isVersion1;
+}
+public String getUserName() {
+	return this.userName;
+}
+public String getPassword() {
+	return this.password;
+}
+public String getMailId() {
+	return this.mailId;
+}
+public long getContactNumber() {
+	return this.contactNumber;
+}
+public int getNotification() {
+	return this.notification;
+}
+public int getNumberOfMessages() {
+	return this.numberOfMessages;
+}
+public boolean getProfile() {
+	return this.profile;
+}
+public String getCeo() {
+	return this.ceo;
+}
+public String getFounded() {
+	return this.founded;
+}
+public String getHeadquarters() {
+	return this.headquarters;
+}
+public String getCmo() {
+	return this.cmo;
+}
+public String getFounders() {
+	return this.founders;
+}
+public int getNumberOfFounders() {
+	return this.numberOfFounders;
+}
+public String getSubsidiaries() {
+	return this.subsidiaries;
+}
+public String getParentOrganizationName() {
+	return this.parentOrganizationName;
+}
+public boolean getIsSafety() {
+	return this.isSafety;
+}
+public boolean getColor() {
+	return this.color;
+}
+public int getBlockwebsite() {
+	return this.blockwebsite;
+}
+public int getAllowwebsite() {
+	return this.allowwebsite;
+}
+public int getNumberOfContacts() {
+	return this.numberOfContacts;
+}
+public boolean getStorage() {
+	return this.storage;
+}
+public int getNumberOfGooglePhotos() {
+	return this. numberOfGooglePhotos;
+}
+public short getNumberOfGoogleAccounts() {
+	return this.numberOfGoogleAccounts;
+}
+public short getTotalApps() {
+	return this.totalApps;
+}
+public boolean getIsVersion() {
+	return this.isVersion;
+}
+public String getVersionName() {
+	return this.versionName;
+}
+public int getLoginTime() {
+	return this.loginTime;
+}
+public String getUserName1() {
+	return this.userName1;
+}
+public String getPassword1() {
+	return this.password1;
+}
+public String getMailId1() {
+	return this.mailId1;
+}
+public long getContactNumber1() {
+	return this.contactNumber1;
+}
+public int getNotification1() {
+	return this.notification1;
+}
+public int getNumberOfMessages1() {
+	return this.numberOfMessages1;
+}
+public boolean getProfile1() {
+	return this.profile1;
+}
+public String getCeo1() {
+	return this.ceo1;
+}
+public String getFounded1() {
+	return this.founded1;
+}
+public String getHeadquarters1() {
+	return this.headquarters1;
+}
+public String getCmo1() {
+	return this.cmo1;
+}
+public String getFounders1() {
+	return this.founders1;
+}
+public int getNumberOfFounders1() {
+	return this.numberOfFounders1;
+}
+public String getSubsidiaries1() {
+	return this.subsidiaries1;
+}
+public String getParentOrganizationName1() {
+	return this.parentOrganizationName1;
+}
+public boolean getIsSafety1() {
+	return this.isSafety1;
+}
+public boolean getColor1() {
+	return this.color1;
+}
+public int getBlockwebsite1() {
+	return this.blockwebsite1;
+}
+public int getAllowwebsite1() {
+	return this.allowwebsite1;
+}
+public int getNumberOfContacts1() {
+	return this.numberOfContacts1;
+}
+public boolean getStorage1() {
+	return this.storage1;
+}
+public int getNumberOfGooglePhotos1() {
+	return this. numberOfGooglePhotos1;
+}
+public short getNumberOfGoogleAccounts1() {
+	return this.numberOfGoogleAccounts1;
+}
+public short getTotalApps1() {
+	return this.totalApps1;
+}
+public boolean getIsVersion1() {
+	return this.isVersion1;
+}
+}
+
