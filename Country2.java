9aa27f66a8c3891a0a613de9f9c5a8d425e74ee7
vanimadevxworkz <vanimadevxworkz@gmail.com>
2024-01-20 08:35:37 +0530

    Collection

---
+++ Country2.java
@@ -0,0 +1,124 @@
+package com.xworkz.collection.driver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Country2 {
+	public static void main(String[] args) {
+		
+	List<String> list=new ArrayList<>();
+	list.add("Afaghanistna");
+	list.add("Albania");
+	list.add("Algeriya");
+	list.add("Andoora");
+	list.add("Angulia");
+	list.add("Argentina");
+	list.add("Austrelia");
+	list.add("belize");
+	list.add("Bhanlagadesh");
+	list.add("Beligam");
+	list.add("Bhutan");
+	list.add("Brazil");
+	list.add("Maynmar");
+	list.add("cambodia");
+	list.add("cameroen");
+	list.add("canada");
+	list.add("colombia");
+	list.add("comoroes");
+	list.add("congo");
+	list.add("cuba");
+	list.add("cyprus");
+	list.add("dominica");
+	list.add("Egypt");
+	list.add("france");
+	list.add("germany");
+	list.add("ghana");
+	list.add("great britan");
+	list.add("greece");
+	list.add("guinea");
+	list.add("guyaan");
+	list.add("haiti");
+	list.add("hondurus");
+	list.add("hungary");
+	list.add("India");
+	list.add("Indoesia");
+	list.add("iran");
+	list.add("iraq");
+	list.add("isrel");
+	list.add("italy");
+	list.add("japan");
+	list.add("kenya");
+	list.add("jordan");
+	list.add("kuwit");
+	list.add("laos");
+	list.add("lebanon");
+	list.add("libya");
+	list.add("lithuania");
+	list.add("malawi");
+	list.add("maldvies");
+	list.add("malaysia");
+	list.add("manaco");
+	list.add("Afaghanistna");
+	list.add("Albania");
+	list.add("Algeriya");
+	list.add("Andoora");
+	list.add("Angulia");
+	list.add("Argentina");
+	list.add("Austrelia");
+	list.add("belize");
+	list.add("Bhanlagadesh");
+	list.add("Beligam");
+	list.add("Bhutan");
+	list.add("Brazil");
+	list.add("Maynmar");
+	list.add("cambodia");
+	list.add("cameroen");
+	list.add("canada");
+	list.add("colombia");
+	list.add("comoroes");
+	list.add("congo");
+	list.add("cuba");
+	list.add("cyprus");
+	list.add("dominica");
+	list.add("Egypt");
+	list.add("france");
+	list.add("germany");
+	list.add("ghana");
+	list.add("great britan");
+	list.add("greece");
+	list.add("guinea");
+	list.add("guyaan");
+	list.add("haiti");
+	list.add("hondurus");
+	list.add("hungary");
+	list.add("India");
+	list.add("Indoesia");
+	list.add("iran");
+	list.add("iraq");
+	list.add("isrel");
+	list.add("italy");
+	list.add("japan");
+	list.add("kenya");
+	list.add("jordan");
+	list.add("kuwit");
+	list.add("laos");
+	list.add("lebanon");
+	list.add("libya");
+	list.add("lithuania");
+	list.add("malawi");
+	list.add("maldvies");
+	
+	//for each
+	for(String countryList:list) {
+		System.out.println(countryList);
+	}
+	System.out.println(list.size());
+	
+	System.out.println("sub list:"+list.subList(10, 20));
+	
+	
+	
+	
+	}
+
+}
---
+++ Movies2.java
@@ -0,0 +1,74 @@
+package com.xworkz.collection.driver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movies2 {
+	public static void main(String[] args) {
+		List<String> movieList=new ArrayList<>();
+		
+		movieList.add("Mejestic");
+		movieList.add("Dhurva");
+		movieList.add("Ningoskra");
+		movieList.add("Kitti");
+		movieList.add("Kariya");
+		movieList.add("Lalihadu");
+		movieList.add("Niinandre ista");
+		movieList.add("Lankesh patrike");
+		movieList.add("Namma preetiya ramu");
+		movieList.add("Dasa");
+		movieList.add("Dahrma");
+		movieList.add("Annavru");
+		movieList.add("Dahrshn");
+		movieList.add("Bhagvan");
+		movieList.add("Kalasipalya");
+		movieList.add("Sardra");
+		movieList.add("Ayya");
+		movieList.add("Swami");
+		movieList.add("Mandya");
+		movieList.add("Suntragali");
+		movieList.add("Datta");
+		movieList.add("Tangigagi");
+		movieList.add("Jotejoteyali");
+		movieList.add("Bhupati");
+		movieList.add("Anatharu");
+		movieList.add("Snehana pretina");
+		movieList.add("I bandhna");
+		movieList.add("Gaja");
+		movieList.add("Indra");
+		movieList.add("Arjun");
+		movieList.add("Navagrha");
+		movieList.add("Yodha");
+		movieList.add("abhaya");
+		movieList.add("Porki");
+		movieList.add("Shourya");
+		movieList.add("Bass");
+		movieList.add("Prince");
+		movieList.add("Sarthi");
+		movieList.add("chinhgari");
+		movieList.add("karntiveera sangollirayanna");
+		movieList.add("Bulbul");
+		movieList.add("Bhurndava");
+		movieList.add("amberresh");
+		movieList.add("Mr Iravata");
+		movieList.add("Virat");
+		movieList.add("Jaggudada");
+		movieList.add("Chakravatrti");
+		movieList.add("Tark");
+		movieList.add("yajamana");
+		movieList.add("katera");
+		
+		
+		for(String list:movieList) {
+			System.out.println("movie list:"+list);
+			
+		}
+		System.out.println("sub list:"+movieList.subList(1, 10));
+		
+		//System.out.println(movieList.equals("katrea"));
+		//System.out.println(movieList.set(0, "kranti"));
+		//movieList.clear();
+		
+	}
+
+}
